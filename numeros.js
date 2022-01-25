9fe30f7a1ba163ca925401a9b3250f9dfeb11604
Brian <rodriguezrbrian@gmail.com>
2022-01-25 20:56:33 -0300

    borré todo

---
+++ numeros.js
@@ -0,0 +1,9 @@
+let numeros = [0, 1, 2, 3, 4];
+let suma = (nroA, nroB) => nroA + nroB;
+
+let nombre = Camila
+let hola = "Saludo a" + nombre
+
+//hola camila
+
+console.log(hola + nombre);
---
+++ prueba.js
@@ -0,0 +1,1 @@
+let prueba = ["A", "B", "C"]
